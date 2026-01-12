1cad15a981b5ce69f06ab60cf68543bf6bd4e026
velu <mayainteriors.in@gmail.com>
2026-01-12 23:08:20 +0530

    Revert "Fix: Remove window.syncCabinetConfigFrontLaminate hack from main.tsx"
    This reverts commit 18676a7e1aaac78e0dfd6a7d886f6873571787ad.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,18 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-// PATCH 35: Global Error Boundary
-import ErrorBoundary from '@/components/system/ErrorBoundary'
-// PATCH 49: Production safety boot check
-import { runBootCheck } from '@/lib/system/bootCheck'
-
-// PATCH 49: Validate critical config on startup
-runBootCheck();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary title="CutListCreator crashed">
-      <App />
-    </ErrorBoundary>
+    <App />
   </StrictMode>,
 )
